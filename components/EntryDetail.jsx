e33b20413f01295ff52be23eff91841073f98d60
Aziz Shad <aziz_ahmadshad@yahoo.com>
2025-09-24 15:35:04 +0200

    Components added

---
+++ components/EntryDetail.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const EntryDetail = () => {
+  return <div>EntryDetail</div>;
+};
+
+export default EntryDetail;
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import EntriesList from "../components/EntriesList";
+import AddEntry from "../components/AddEntry";
 function App() {
   return (
-    <>
-      <h1>Personal Dairy</h1>
-    </>
+    <main className="w-1/3 text-center mx-auto p-4 min-h-screen border-1 border-[#4F342F] shadow-lg shadow-[#4F342F] rounded-lg">
+      <h1 className="text-6xl font-sans text-[#4F342F]">Personal Dairy</h1>
+      <div className="flex justify-start">
+        <AddEntry />
+      </div>
+
+      <div className="border-1 border-[#4F342F] mt-3 mb-5 w-full mx-auto"></div>
+
+      <EntriesList />
+      <EntriesList />
+      <EntriesList />
+      <EntriesList />
+      <EntriesList />
+    </main>
   );
 }
 
 export default App;
+//<!--  #4F342F -->
